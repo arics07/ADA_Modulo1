6d4ec16579c0bed6874ebd01af01fe0b90d3038e
Ariadna Salomone <aricsalomone@gmail.com>
2025-06-04 00:49:17 -0300

    Subo ejercicios 8 y 10

---
+++ Clase 10/ejercicio8.js
@@ -0,0 +1,22 @@
+/*
+
+•	Ejercicio 8 - Conversión de Temperaturas: Escribe un programa que convierta una 
+temperatura dada en grados Celsius a grados Fahrenheit. La fórmula de conversión es: 
+
+ celsius * 9 / 5 + 32
+
+Pide al usuario que ingrese la temperatura en Celsius y muestra el resultado en Fahrenheit.
+	Pista1: Investiga que es ParseFloat, ya que el ejercicio lo requiere
+Pista 2: La fórmula puede representarse asi: 
+let fahrenheit = celsius * 9 / 5 + 32; 
+
+*/
+
+//para poder ingresar informacion por teclado
+const prompt = require('prompt-sync')();
+
+let temperaturaCelsius = parseFloat(prompt("Ingrese la temperatura en °C: "));
+
+let fahrenheit = temperaturaCelsius * 9/5 + 32;
+
+console.log("La temperatura en Fahrenheit es: " + fahrenheit);
