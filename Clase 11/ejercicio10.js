276e676086d97afef91cc3a74fc1a61d61750cd9
Ariadna Salomone <aricsalomone@gmail.com>
2025-06-09 20:04:44 -0300

    Subo ejercicios Clase 11

--- Clase 11/ejercicio10.js
+++ Clase 11/ejercicio10.js
@@ -9,4 +9,31 @@ Usa un for para resolver este ejercicio.
 
 */
 
+const prompt = require('prompt-sync')();
 
+const NUMERO_SECRETO = Math.floor(Math.random() * 10) + 1;
+
+let acierto = false;
+
+for (let intento=1; intento<=3; intento++) {
+    const ADIVINANZA = parseInt(prompt(`Intento ${intento}: Adivina el número del 1 al 10... `));
+
+    // estructura switch para evaluar la entrada del usuario
+    switch (true) {
+        case (ADIVINANZA === NUMERO_SECRETO):
+            console.log("Adivinaste el número. GANASTE!!!");
+            acierto = true;
+            break;
+        case (ADIVINANZA < NUMERO_SECRETO):
+            console.log("Muy bajo. Intentá un número más alto... ");
+            break;
+        case (ADIVINANZA > NUMERO_SECRETO):
+            console.log("Muy alto. Intentá con un número más bajo... ");
+            break;
+    }
+
+    // operador ternario para cortar el bucle si acertó
+    if (acierto ? true : false) break;
+}
+
+if (!acierto) console.log(`El número secreto era ${NUMERO_SECRETO}`)
